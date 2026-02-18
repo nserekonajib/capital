c8d4d1670df293cabf51606dbfee8fa2925769bf
LUNSERK TECHNOLOGIES <109471084+nserekonajib@users.noreply.github.com>
2026-02-18 15:51:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,8 @@ def internal_error(error):
 if __name__ == '__main__':
     # Set up logging
     # logging.basicConfig(level=logging.INFO)
-    app.run(debug=True, port=3000)
+    app.run(host = "0.0.0.0", debug=True, port=3000)
+
 
 
 
